d4111efc744e242ce27aaf31e314c1a33599edbb
Genomeee <mishasydora@gmail.com>
2019-12-20 00:07:31 +0200

    add features

---
+++ db/migrate/20191211011807_migrate_all.rb
@@ -0,0 +1,32 @@
+class MigrateAll < ActiveRecord::Migration[5.2]
+  def change
+    create_table :studios do |t|
+      t.string :name
+      t.string :founder
+
+      t.timestamps
+    end
+
+    create_table :games do |t|
+      t.string :name
+      t.integer :price
+      t.string :genre
+      t.belongs_to :studio, index: { unique: true }, foreign_key: true
+      t.belongs_to :system_requirement, index: { unique: true }, foreign_key: true
+
+      t.timestamps
+    end
+
+    create_table :system_requirements do |t|
+      t.string :system
+      t.integer :memory
+      t.string :processor
+      t.string :graphicsCard
+      t.integer :space
+      t.belongs_to :game, index: { unique: true }, foreign_key: true
+
+      t.timestamps
+    end
+
+  end
+end
